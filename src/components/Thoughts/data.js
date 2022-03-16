f1b5e21b9c625605de59665997147245307adf0f
Veronica Harris <veronica.n.harris92@gmail.com>
2022-03-16 12:38:31 -0400

    adding slide show with quotes

---
+++ src/components/Thoughts/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        "quote":  "I also want to share memories of his reactions during the NFL season. In his heart he knew and never doubted that Tampa was going back to the superbowl. He was very aware of all the position players on the team and the reasons they were going to beat the Rams in the playoff game. He was a true fan, in every sense of the word. The Bucs and our class were lucky to have him as long as we did. He was a pleasure to have in class.",
+        "person": "John Dinsmore - Instructor",
+    },
+    {
+        "quote": "Carlos and I became quite close throughout the bootcamp. I still remember the day I asked him to join our group. We had a fun time doing an activity, and coincidentally, it was the same day the instructor asked us to start thinking about who we should partner with. Since then, we have shared many great memories. Most of our conversations were about coding-related topics. I am not into sports and he was not into the videogames I was into. However, we worked super hard on both our projects and we were VERY proud of our first project. After the first project, we often studied together and practiced the class activities. We always laughed together when working/studying and often just talked afterwards. We were extremely excited to do the third project together. I hope this bootcamp helped him ease some of the burden that he had been carrying. I hope my group and I had as much of a positive impact on him as he did on us. ",
+        "person": "Satya Mehta - Classmate/Group Member",
+    },
+    {
+        "quote": "Carlos was always a great source of help and knowledge to his classmates. He was a joy to work with and have as a student. He called me into one of the breakout rooms and would ask me great questions that would always keep my knowledge sharp. He will always be one of my best students.",
+        "person": "Veronica Harris - TA",
+    },
+    {
+        "quote": "Carlos was a good friend and 2 of our other group members are really good friends with Carlos. We Had plans to hangout after our boot camp was over.He was really smart and funny. We talked about many things like music, investing, coding and just things we’ve done like what friends do. He helped me with a lot of topics in class after class hours in our study group. Carlos had a positive impact on me during this boot camp and I can say without his help understanding concepts I don’t know where I would be in this boot camp without our study groups.",
+        "person": "Neal Anand - Classmate/Group Member",
+    },
+    // {
+    //     "quote": "",
+    //     "person": "",
+    // },
+]
--- src/components/Thoughts/index.js
+++ src/components/Thoughts/index.js
@@ -1,48 +1,18 @@
 import React, {useState} from "react";
 import "./style.css";
-import { Carousel } from "bootstrap";
+// import { Carousel } from "bootstrap";
+import {data} from "./data";
 
-function Thoughts() {
-  
-  
+const Thoughts = () => 
+  const [current, setCurrent] = useState(0)
+  const length = data.length
+
     return (
-        <div>
-          {/* <Carousel>
-            <Carousel.Item interval={1000}>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Second slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel> */}
-      </div>
+        <section className="slider">
+          {data.map((data, index) => {
+            return <p className="quotes">{data.quote} {data.person}</p>;
+          })}
+        <section/>
     );
 }
 
