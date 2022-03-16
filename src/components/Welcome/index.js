941cc7cde101d7fb3a74c8aefc7b2edd1c1456c7
Veronica Harris <veronica.n.harris92@gmail.com>
2022-03-16 12:06:42 -0400

    edited the styling for footer and welcome, will not be working on thoughts

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -5,9 +5,9 @@ import "./style.css"
 export default function Welcome() {
 
     return (
-        <div>
+        <div className="stylish">
             <h1>In Loving Memory of Carlos Simon</h1>
-            <p className="welcome">Welcome to the Carlos Simon memorial website. He began his coding journey in November 2021, and was taken too soon. The entire coding bootcamp class thought it would be proper to honor and celebrate his life through the thing that brought us all together: Coding. See below for some loving thoughts and memories about Carlos from each of his classmates. Thank you.</p>
+            <p className="welcome">Welcome to the Carlos Simon memorial website. He began his coding journey in November 2021, and was taken too soon. The entire coding bootcamp class thought it would be proper to honor and celebrate his life through the thing that brought us all together: Coding. See below for some loving thoughts and memories about Carlos from each of his classmates and instructional staff members. Thank you.</p>
         </div>
     )
     
